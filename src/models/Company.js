94cc09dacb329f2804a60d2bbeca169cabe6e7e1
pedropsoares <pedropaulos.1998@gmail.com>
2020-04-16 14:31:59 -0300

    company model finalized

--- src/models/Company.js
+++ src/models/Company.js
@@ -3,6 +3,10 @@ const mongoose = require('mongoose');
 const CompanySchema = new mongoose.Schema({
   name: String,
   cnpj: String,
+  password: {
+    type: String,
+    select: false
+  },
   bio: String,
   techs: [String],
   recruiters: [
